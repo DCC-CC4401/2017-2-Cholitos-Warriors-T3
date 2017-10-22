0742f064f1ab47e7c79898a200356861dbf2d34b
Daniton2906 <ddiomedi2906@gmail.com>
2017-10-22 04:51:41 -0300

    Add Adoption Form App to settings

--- Tarea3/settings.py
+++ Tarea3/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Cholito.apps.CholitoConfig',
+    'users.apps.NameConfig',
+    'adoption.apps.AdoptionConfig',
 ]
 
 MIDDLEWARE = [
