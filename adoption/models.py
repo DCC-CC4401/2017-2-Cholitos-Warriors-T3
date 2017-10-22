22b6a8e2b5a8258b45ee0c4fab672a290f2e9ac2
Daniton2906 <ddiomedi2906@gmail.com>
2017-10-22 04:48:37 -0300

    Add AdoptionForm model, just a draft

--- adoption/models.py
+++ adoption/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class AdoptionForm (models.Model):
+    """
+        AdoptionForm model.
+        It has:
+        - name
+        - date from it is in adoption
+        - type of animal
+        - gender
+        - description
+        - if the animal's been adopted or not
+    """
+
+    name = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
+    in_adoption_from = models.DateTimeField()
+    description = models.TextField(max_length=140)
+    adopted = models.BooleanField(False)
+
