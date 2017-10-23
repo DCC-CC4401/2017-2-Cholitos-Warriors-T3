964bb21d98ed3a48571ffb6bf143f841df381f0c
Daniton2906 <ddiomedi2906@gmail.com>
2017-10-22 22:38:43 -0300

    Create report table for municipalities according the report table Add views to municipalidad/views.py Small changes

--- municipalidad/views.py
+++ municipalidad/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from denuncias.models import Denuncia
 
 # Create your views here.
 def stats_denuncias(request):
     return render(request, 'muni-estadisticas-denuncias.html')
 
-# Create your views here.
 def stats_fichas(request):
     return render(request, 'muni-estadisticas-ongs-fichas.html')
 
-# Create your views here.
 def denuncias_recibidas(request):
     return render(request, 'muni-denuncias-recibidas.html')
+
+class ListaDenuncias(ListView):
+
+    template_name = 'muni-denuncias-recibidas.html'
+
+    def get(self, request, **kwargs):
+        return render(request, self.template_name, {'denuncias': Denuncia.objects.all()})
+
+
