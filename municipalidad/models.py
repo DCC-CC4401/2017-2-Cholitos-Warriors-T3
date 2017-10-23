5e6d1260bc71b6ddd278e929e23bc23891241523
Daniton2906 <ddiomedi2906@gmail.com>
2017-10-22 21:34:19 -0300

    Add actual_state attribute to Denuncia Model Make ManyToOne relationship between Denuncia and Place Models Small changes

--- municipalidad/models.py
+++ municipalidad/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Municipalidad(models.Model):
     name = models.CharField(max_length=50)
-    comuna = models.CharField(max_length=30, unique=True)
+    comuna = models.CharField(max_length=30, unique=True, null=True)
 
     def __str__(self):
         return self.comuna
