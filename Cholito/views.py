8360aa95e287fb59f9fd227841e54bb034c9aadd
Daniton2906 <ddiomedi2906@gmail.com>
2017-10-23 08:31:23 -0300

    Move login.html and signup.html to users/templates

--- Cholito/views.py
+++ Cholito/views.py
@@ -13,19 +13,20 @@ def landingPage(request):
         'ONGData':ongs
     }
 
-    if  request.user.is_authenticated():
-        return render(request, 'usuario-in-adoptar.html', context)
+    if request.user.is_authenticated():
+        return render(request, "muni-estadisticas-denuncias.html")#'usuario-in-adoptar.html', context)
 
     else:
         return render(request, 'usuario-out-adoptar.html', context)
 
+
 def denuncia(request):
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         typeOfAbuse = request.POST['typeOfAbuse']
         animal = request.POST['animal']
         hurt = False
         gender = request.POST['sexo']
-        if(request.POST['herido'] == 'si'):
+        if request.POST['herido'] == 'si':
             hurt = True
         color = request.POST['color']
         comment = request.POST['comentario']
@@ -34,4 +35,6 @@ def denuncia(request):
         lugar.save()
         denuncia = Denuncia(kindOfAbuse=typeOfAbuse, kindOfAnimal=animal, gender=gender, colour=color, hurt=hurt, comments=comment, location=lugar)
         denuncia.save()
+    else:
+        return redirect('/')
     return landingPage(request)
