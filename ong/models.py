bc9d1990fec5f7e8decef8ed0146b280434c8970
Lxopato <lpbustoscarrasco@gmail.com>
2017-10-23 16:11:18 -0300

    Increased name max length, add address field

--- ong/models.py
+++ ong/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class ONG(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     user = models.OneToOneField(User, null=True)
+    direccion = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to="municipalidad/profile_pictures", null=True)
 
     def __str__(self):
